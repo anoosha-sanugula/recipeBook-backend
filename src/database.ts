659c863f36e8b5f8921240f3dca6208032c4ddb6
anoosha-sanugula <anoosha.sanugula@everest.engineering>
2024-12-12 06:55:54 +0530

    Create sequelize connection

---
+++ src/database.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection to PostgreSQL has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+export { sequelize, connectToDatabase };
