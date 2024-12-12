8b78d7c9bc8614cfc5c02bdc1977b38aa767f17e
anoosha-sanugula <anoosha.sanugula@everest.engineering>
2024-12-13 00:08:36 +0530

    Add user routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import { sequelize } from "./server";
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/user/user";
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 export const main = async () => {
   try {
-    await sequelize.sync({ force: false });
+    const PORT = process.env.PORT || 3002;
     await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
     console.log("Connection has been established successfully.");
+
+    app.use("/recipebook", userRoutes);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   } catch (error: any) {
     throw new Error(`Unable to connect to the database: ${error.message}`);
   }
