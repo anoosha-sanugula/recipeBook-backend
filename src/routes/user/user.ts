e8dbaba1c35cf122ea77bff69671e362ea985d82
anoosha-sanugula <anoosha.sanugula@everest.engineering>
2024-12-13 00:35:44 +0530

    Add get route

--- src/routes/user/user.ts
+++ src/routes/user/user.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { postUser } from "../../controllers/user/postUser";
+import { getUser } from "../../controllers/user/getUser";
 const userRoutes = express();
 userRoutes.use(express.urlencoded({ extended: true }));
 userRoutes.use(express.json());
 
 userRoutes.post("/users", postUser);
+userRoutes.get("/users", getUser);
 export default userRoutes;
