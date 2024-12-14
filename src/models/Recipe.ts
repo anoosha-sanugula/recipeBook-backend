104110816556ada02871b57050d24877aa469703
anoosha-sanugula <anoosha.sanugula@everest.engineering>
2024-12-14 21:16:29 +0530

    Add some more fields in recipe model

--- src/models/Recipe.ts
+++ src/models/Recipe.ts
@@ -1,9 +1,37 @@
-import { DataTypes } from "sequelize";
-import { User } from "../models/User";
+import { DataTypes, Model, Optional } from "sequelize";
 import { sequelize } from "../server";
 
-const Recipe = sequelize.define(
-  "Recipe",
+interface RecipeAttributes {
+  id: number;
+  title: string;
+  description?: string;
+  instructions?: string;
+  category: "Breakfast" | "Lunch" | "Dinner" | "Dessert";
+  rating?: number;
+  imageUrl?: string;
+  videoUrl?: string;
+  cookingTime?: number;
+  nutritionFact?: string;
+}
+
+interface RecipeCreationAttributes extends Optional<RecipeAttributes, "id"> {}
+export class Recipe
+  extends Model<RecipeAttributes, RecipeCreationAttributes>
+  implements RecipeAttributes
+{
+  public id!: number;
+  public title!: string;
+  public description?: string;
+  public instructions?: string;
+  public category!: "Breakfast" | "Lunch" | "Dinner" | "Dessert";
+  public rating?: number;
+  public imageUrl?: string;
+  public videoUrl?: string;
+  public cookingTime?: number;
+  public nutritionFact?: string;
+}
+
+Recipe.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -20,23 +48,37 @@ const Recipe = sequelize.define(
     },
     instructions: {
       type: DataTypes.TEXT,
-      allowNull: false,
+      allowNull: true,
     },
     category: {
+      type: DataTypes.ENUM("Breakfast", "Lunch", "Dinner", "Dessert"),
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    imageUrl: {
       type: DataTypes.STRING,
+      allowNull: true,
+    },
+    videoUrl: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    cookingTime: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: "Cooking time in minutes",
+    },
+    nutritionFact: {
+      type: DataTypes.TEXT,
       allowNull: true,
     },
   },
   {
-    timestamps: true,
+    sequelize,
+    tableName: "Recipes",
+    timestamps: false,
   }
 );
-
-Recipe.belongsTo(User, {
-  foreignKey: {
-    allowNull: false,
-  },
-  onDelete: "CASCADE",
-});
-
-module.exports = Recipe;
