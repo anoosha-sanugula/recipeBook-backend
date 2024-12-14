f36188700f1bc8c4f1cc68f0d0229c3a7c2722c0
anoosha-sanugula <anoosha.sanugula@everest.engineering>
2024-12-14 10:40:47 +0530

    Add a encryption method

--- src/classes/user/user.ts
+++ src/classes/user/user.ts
@@ -1,18 +1,27 @@
+import argon2 from "argon2";
 export class User {
   username: string;
   email: string;
   password: string;
-  profileImage: string;
+  country: string;
 
   constructor(
     username: string,
     email: string,
     password: string,
-    profileImage: string
+    country: string
   ) {
     this.username = username;
     this.email = email;
     this.password = password;
-    this.profileImage = profileImage;
+    this.country = country;
   }
+  encryptPassword = async () => {
+    try {
+      const hash = await argon2.hash(this.password);
+      this.password = hash;
+    } catch (err) {
+      throw new Error("Error while password encryption");
+    }
+  };
 }
