c30e01d29a54fb974d93b91e108480a07c150883
anoosha-sanugula <anoosha.sanugula@everest.engineering>
2024-12-15 17:30:42 +0530

    Add get recipe by id route

--- src/routes/recipe/recipe.ts
+++ src/routes/recipe/recipe.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { createRecipe } from "../../controllers/recipe/createRecipes";
 import { getRecipes } from "../../controllers/recipe/getRecipes";
+import { getRecipeById } from "../../controllers/recipe/getRecipeById";
 const recipeRoutes = express();
 recipeRoutes.use(express.urlencoded({ extended: true }));
 recipeRoutes.use(express.json());
 
 recipeRoutes.post("/recipes", createRecipe);
 recipeRoutes.get("/recipes", getRecipes);
+recipeRoutes.get("/recipes/:recipeId", getRecipeById);
 export default recipeRoutes;
