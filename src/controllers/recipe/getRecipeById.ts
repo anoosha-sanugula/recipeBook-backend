c377e04f8f2aa19ef30cde41bc42e72bb7716c52
anoosha-sanugula <anoosha.sanugula@everest.engineering>
2024-12-15 17:32:03 +0530

    Create get recipe by id route

---
+++ src/controllers/recipe/getRecipeById.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { Recipe } from "../../models/Recipe";
+import { Ingredients } from "../../models/Ingredients";
+
+export const getRecipeById = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  const { recipeId } = req.params;
+
+  try {
+    const recipe = await Recipe.findOne({
+      where: {
+        id: recipeId,
+      },
+      include: {
+        model: Ingredients,
+        attributes: ["name", "quantity", "unit", "imageUrl"],
+        required: false,
+      },
+    });
+
+    if (!recipe) {
+      return res.status(404).json({ error: "Recipe not found" });
+    }
+    return res.status(200).json(recipe);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
